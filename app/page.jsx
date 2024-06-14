9014a004db0419bd962b90a5404bcf41718ebe7e
mat13 <mathieu.schmitt13@icloud.com>
2024-06-15 01:05:58 +0200

    last

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello i'm <br /> <span className="text-accent">Mathieu Schmitt</span>
+              Helloi&apos;m <br /> <span className="text-accent">Mathieu Schmitt</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
             Je suis un développeur web passionné par la programmation et le design. J&apos;aime créer des sites web
