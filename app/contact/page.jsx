49c1cc9011ced501f7dfe61889fd3fe72a0dde02
mat13 <mathieu.schmitt13@icloud.com>
2024-06-15 00:56:57 +0200

    resolution

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -60,7 +60,7 @@ const Contact = () => {
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
+            <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
               <p className="text-white/60">
                 Veuillez me contacter pour tout vos sites web que ce soit du front end ainsi que le back end <br />
                 je suis destiné à vos besoin en tant que developpeur web fullstack 
